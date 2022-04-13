4c0793a5c2fbcdb78aa7d3d090060dfefb83c247
Yuki Zhao <47805855+yukizyx@users.noreply.github.com>
2022-04-13 17:58:59 -0400

    Add files via upload

--- assignment3.c
+++ assignment3.c
@@ -8,6 +8,47 @@
 
 #define REQUEST_NUM 20
 
+int distance(int a, int b)
+{
+    if (a > b)
+    {
+        return a - b;
+    }
+    else
+    {
+        return b - a;
+    }
+}
+
+void FCFS(int head, int req[])
+{
+    int i;
+    int current = head;
+    int total = 0;
+    int visit[REQUEST_NUM];
+
+    // visit order is the order of the request array
+    for (i = 0; i < REQUEST_NUM; i++)
+    {
+        total += distance(head, req[i]);
+
+        // change & record the num of next request
+        head = req[i];
+        visit[i] = head;
+    }
+
+    // print the visit array
+    for (i = 0; i < REQUEST_NUM - 1; i++)
+    {
+        printf("%d, ", visit[i]);
+    }
+    printf("%d\n", visit[REQUEST_NUM - 1]);
+    printf("\n");
+
+    // print the total movements
+    printf("FCFS - Total head movements = %d\n", total);
+};
+
 int main(int argc, const char *argv[])
 {
     // handle 2 command line args
@@ -34,11 +75,14 @@ int main(int argc, const char *argv[])
     printf("Total requests = %d\n", REQUEST_NUM);
     printf("Initial Head Position: %d\n", head);
     printf("Direction of Head: %s\n", str);
+    printf("\n");
 
     // FCFS
     printf("FCFS DISK SCHEDULING ALGORITHM\n");
+    printf("\n");
 
-    printf("FCFS - Total head movements = %d", fcfs_mv);
+    FCFS(head, request);
+    printf("\n");
 
     // SSTF
     printf("SSTF DISK SCHEDULING ALGORITHM\n");
