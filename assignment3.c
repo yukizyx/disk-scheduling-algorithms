bdfcddd8467b4549cb0115a453bab46e936a964d
Yuki Zhao <47805855+yukizyx@users.noreply.github.com>
2022-04-14 13:49:15 -0400

    Add files via upload

--- assignment3.c
+++ assignment3.c
@@ -7,6 +7,7 @@
 #include <fcntl.h>
 
 #define REQUEST_NUM 20
+#define MAX_POSITION 299
 
 int distance(int a, int b)
 {
@@ -23,7 +24,6 @@ int distance(int a, int b)
 void FCFS(int head, int req[])
 {
     int i;
-    int current = head;
     int total = 0;
     int visit[REQUEST_NUM];
 
@@ -50,8 +50,8 @@ void FCFS(int head, int req[])
 };
 
 void SSTF(int head, int req[])
-{   
-    //init variables
+{
+    // init variables
     int current_pos = head;
     int total = 0;
     int index = 0;
@@ -60,20 +60,20 @@ void SSTF(int head, int req[])
     int candidate = -1;
     int visit_seq[REQUEST_NUM];
 
-    //populate visited array to 0(false)
+    // populate visited array to 0(false)
     for (size_t i = 0; i < REQUEST_NUM; i++)
     {
         visited[i] = 0;
     }
-    
-    //main loop
+
+    // main loop
     for (size_t i = 0; i < REQUEST_NUM; i++)
     {
         min = 9999999;
-        //finding the minimum distance to head
+        // finding the minimum distance to head
         for (size_t j = 0; j < REQUEST_NUM; j++)
         {
-            if ( ! visited[j] && min > distance(head, req[j]))
+            if (!visited[j] && min > distance(head, req[j]))
             {
                 candidate = j;
                 min = distance(head, req[j]);
@@ -96,12 +96,102 @@ void SSTF(int head, int req[])
     printf("\n");
 
     // print the total movements
-    printf("FCFS - Total head movements = %d\n", total);
-
+    printf("SSTF - Total head movements = %d\n", total);
 }
 
-void LOOK(int head, int req[], int left){
+void SCAN(int head, int req[], int left)
+{
+    int index;
+    int i;
+    int total = 0;
+    int visit[REQUEST_NUM];
+    int visit_index = 0;
 
+    // the start direction is left
+    if (left == 1)
+    {
+        // find position (index) of head
+        for (i = 0; i < REQUEST_NUM; i++)
+        {
+            if (head < req[i])
+            {
+                index = i;
+                break;
+            }
+        }
+
+        // move from head to the most left position
+        for (i = index - 1; i >= 0; i--)
+        {
+            total += distance(head, req[i]);
+            head = req[i];
+            visit[visit_index] = head;
+            visit_index++;
+        }
+
+        // move from the most left position to 0
+        total += distance(head, 0);
+        head = 0;
+
+        // move from 0 to the most right position
+        for (i = index; i < REQUEST_NUM; i++)
+        {
+            total += distance(head, req[i]);
+            head = req[i];
+            visit[visit_index] = head;
+            visit_index++;
+        }
+    }
+    // the start direction is right
+    else
+    {
+        // find position (index) of head
+        for (i = 0; i < REQUEST_NUM; i++)
+        {
+            if (head <= req[i])
+            {
+                index = i;
+                break;
+            }
+        }
+
+        // move from head to the most right position
+        for (i = index; i < REQUEST_NUM; i++)
+        {
+            total += distance(head, req[i]);
+            head = req[i];
+            visit[visit_index] = head;
+            visit_index++;
+        }
+
+        // move from the most right position to max
+        total += distance(head, MAX_POSITION);
+        head = MAX_POSITION;
+
+        // move from max to the most left position
+        for (i = index - 1; i >= 0; i--)
+        {
+            total += distance(head, req[i]);
+            head = req[i];
+            visit[visit_index] = head;
+            visit_index++;
+        }
+    }
+
+    // print the visit array
+    for (i = 0; i < REQUEST_NUM - 1; i++)
+    {
+        printf("%d, ", visit[i]);
+    }
+    printf("%d\n", visit[REQUEST_NUM - 1]);
+    printf("\n");
+
+    // print the total movements
+    printf("SCAN - Total head movements = %d\n", total);
+}
+
+void LOOK(int head, int req[], int left)
+{
 }
 
 int main(int argc, const char *argv[])
@@ -118,22 +208,22 @@ int main(int argc, const char *argv[])
     int head = atoi(argv[1]);
     const char *str = argv[2];
 
-    //if is LEFT, then set the value to 1, if RIGHT than 0, other value will result a termination of program
+    // if is LEFT, then set the value to 1, if RIGHT than 0, other value will result a termination of program
     int left = -1;
     if (strcmp(str, "LEFT") == 0)
     {
         left = 1;
-    }else if(strcmp(str, "RIGHT") == 0)
+    }
+    else if (strcmp(str, "RIGHT") == 0)
     {
         left = 0;
-    }else
+    }
+    else
     {
         printf("Invalid 2nd arg, either 'LEFT' or 'RIGHT'");
         return -1;
     }
-    
 
-    
     // read the file and store the int to an array
     FILE *fptr = fopen("request.bin", "rb");
 
@@ -142,24 +232,24 @@ int main(int argc, const char *argv[])
 
     fclose(fptr);
 
-    //use bubble sort to sort request array
+    // use bubble sort to sort request array
     int sorted_req[REQUEST_NUM];
     int temp;
-    //copy array
+    // copy array
     for (size_t i = 0; i < REQUEST_NUM; i++)
     {
         sorted_req[i] = request[i];
     }
-    //inplace bubble sort
+    // inplace bubble sort
     for (size_t i = 0; i < REQUEST_NUM - 1; i++)
     {
         for (size_t j = 0; j < REQUEST_NUM - i - 1; j++)
         {
-            if (sorted_req[j] > sorted_req[j+1]) 
+            if (sorted_req[j] > sorted_req[j + 1])
             {
                 temp = sorted_req[j];
-                sorted_req[j] = sorted_req[j+1];
-                sorted_req[j+1] = temp;
+                sorted_req[j] = sorted_req[j + 1];
+                sorted_req[j + 1] = temp;
             }
         }
     }
@@ -173,9 +263,7 @@ int main(int argc, const char *argv[])
     // FCFS
     printf("FCFS DISK SCHEDULING ALGORITHM\n");
     printf("\n");
-
     FCFS(head, request);
-
     printf("\n");
 
     // SSTF
@@ -186,6 +274,9 @@ int main(int argc, const char *argv[])
 
     // SCAN
     printf("SCAN DISK SCHEDULING ALGORITHM\n");
+    printf("\n");
+    SCAN(head, sorted_req, left);
+    printf("\n");
 
     // C - SCAN
     printf("C-SCAN DISK SCHEDULING ALGORITHM\n");
